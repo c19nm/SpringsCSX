850f1a316a3349fcad5896958fca5501d87a79a7
c19nm <c19nm@dalton.org>
2018-01-10 23:12:22 -0500

    take two

---
+++ Springs/src/SpringsNMKTL/Spring.java
@@ -0,0 +1,99 @@
+//package SpringsNMKTL;
+//
+//import org.opensourcephysics.display.Circle;
+//
+//public class Spring extends Circle {
+//	// extends circle to make the balls circles
+//	// initializes the attributes for a ball
+//	double k = 0; // k constant
+//	double m = 0; // mass
+//	double position = 0; //deltax
+//	double distance = 0; //distance
+//	
+//
+//	public double getK() {
+//		return k;
+//	}
+//
+//
+//
+//
+//	public void setK(double k) {
+//		this.k = k;
+//	}
+//
+//
+//
+//
+//	public double getM() {
+//		return m;
+//	}
+//
+//
+//
+//
+//	public void setM(double m) {
+//		this.m = m;
+//	}
+//
+//
+//
+//
+//	public double getPosition() {
+//		return position;
+//	}
+//
+//
+//
+//
+//	public void setPosition(double position) {
+//		this.position = position;
+//	}
+//
+//
+//
+//
+//	public double getDistance() {
+//		return distance;
+//	}
+//
+//
+//
+//
+//	public void setDistance(double distance) {
+//		this.distance = distance;
+//	}
+//
+//
+//
+//
+//	/**
+//		 * Creates a ball with a unique set of attributes. Ball(x position, y position,
+//		 * x acceleration, y acceleration, x velocity, y velocity, initial velocity,
+//		 * angle).
+//		 * 
+//		 * @param px
+//		 *            x position of the ball at that point in time (is a double)
+//		 * @param py
+//		 *            y position of the ball at that point in time (is a double)
+//		 * @param ax
+//		 *            x acceleration of the ball at that point in time (is a double)
+//		 * @param ay
+//		 *            y acceleration of the ball at that point in time (is a double)
+//		 * @param vx
+//		 *            x velocity of the ball at that point in time (is a double)
+//		 * @param vy
+//		 *            y velocity of the ball at that point in time (is a double)
+//		 * @param v
+//		 *            initial velocity of the ball (is a double)
+//		 * @param theta
+//		 *            angle of the ball relative to the x axis (is a double)
+//		 */
+//		public Spring(double k, double m, double position, double distance) {
+//			this.k = k;
+//			this.m = m;
+//			this.position = position;
+//			this.distance = distance;
+//		}
+//
+//}
