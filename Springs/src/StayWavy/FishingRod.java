7748645657155ef0f2f9c877be7b050d73a818a8
c19nm <c19nm@dalton.org>
2018-05-24 00:09:14 -0400

    Update FishingRod.java

--- Springs/src/StayWavy/FishingRod.java
+++ Springs/src/StayWavy/FishingRod.java
@@ -12,21 +12,18 @@ public class FishingRod {
 	int particleNum;
 	Spring[][] array;
 
-	public FishingRod(double k, double rodlength, int numofParticles, double springmass, double timestep) {
+	public FishingRod(double k, double rodlength, int numofParticles, double springmass, double sideLength,
+			double timestep) {
 		length = rodlength;
 		particleNum = numofParticles;
-		double springLength = rodlength/numofParticles;
-		
-		for (int i = 0; i < particleNum; i++) { 
-			array[i][0] = new Spring(k - 10*i, springmass, 0, i * springLength, 0, 0, 0, 0, timestep);
-			array[i][1] = new Spring(k - 10*i, springmass, 1, i * springLength, 0, 0, 0, 0, timestep);
+		// double springLength = rodlength / numofParticles;
+
+		array = new Spring[particleNum][2];
+
+		for (int i = 0; i < particleNum; i++) {
+			array[i][0] = new Spring(k - 10 * i, springmass, 0, i * sideLength, 0, 0, 0, 0, timestep);
+			array[i][1] = new Spring(k - 10 * i, springmass, 1, i * sideLength, 0, 0, 0, 0, timestep);
 		}
 	}
-	
-
-	
-	
-	
-	
 
 }
